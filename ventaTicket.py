e5759ed069808aba2a1a68b57afdec0a648fa49c
ManuGZ <galonso097@gmail.com>
2023-09-25 23:53:25 -0500

    a

--- ventaTicket.py
+++ ventaTicket.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template, request, redirect
 from flaskext.mysql import MySQL
 from datetime import datetime
+import random as r
 
 app = Flask(__name__)
 
@@ -40,8 +41,9 @@ def guardar_eventos():
     _imagen = request.form['txtImagen']
     _desc = request.form['txtDesc']
     _boletos = request.form['txtBoletos']
+    _precio = request.form['txtPre']
 
-    sql = f"INSERT INTO `sitio`.`eventos` (`nombre`, `imagen`, `descripcion`, `boletos`) VALUES ('{_nombre}', '{_imagen}', '{_desc}', '{_boletos}');"
+    sql = f"INSERT INTO `sitio`.`eventos` (`nombre`, `imagen`, `descripcion`, `boletos`,`precio`) VALUES ('{_nombre}', '{_imagen}', '{_desc}', '{_boletos}','{_precio}');"
     conexion = mysql.connect()
     cursor = conexion.cursor()
     cursor.execute(sql)
@@ -49,13 +51,40 @@ def guardar_eventos():
     return redirect('/eventos')
 
 
-@app.route('/ventas', methods=['POST'])
+@app.route('/ventas')
 def ventas_app():
     conexion = mysql.connect()
     cursor = conexion.cursor()
     cursor.execute("SELECT * FROM sitio.eventos")
     eventos = cursor.fetchall()
     conexion.commit()
+    return render_template('ventas.html', eventos=eventos)
+
+
+@app.route('/ventas/exito', methods=['POST'])
+def ventas_exito():
+    _nombre = request.form['nombre_eventos']
+    _cantidad = request.form['cantidad_boletos']
+    _correo = request.form['correo_']
+    _ciudad = request.form['ciudad_']
+    _codigoPostal = request.form['codigo_postal']
+    codigo = r.randint(0, 2500)
+
+    sql = f"INSERT INTO `sitio`.`ventas` (`codigo`, `correo_electronico`, `nombre_evento`, `ciudad`,`codigo_postal`,`precio`) VALUES ('{codigo}', '{_correo}', '{_nombre}', '{_ciudad}','{_codigoPostal}','{0.0}');"
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
+    cursor.execute(sql)
+    conexion.commit()
+
+    sql = f"UPDATE `sitio`.`eventos` SET `boletos` = `boletos`-'{_cantidad}' WHERE (`nombre` = '{_nombre}');"
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
+    cursor.execute(sql)
+    conexion.commit()
+
+    cursor.execute("SELECT * FROM sitio.eventos")
+    eventos = cursor.fetchall()
+
     return render_template('ventas.html', eventos=eventos)
 
 
