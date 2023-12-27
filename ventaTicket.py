139a822ec7c6deb1971dbf4d84522361b70833d5
ManuGZ <102195489+ManuGZ@users.noreply.github.com>
2023-12-27 10:45:19 -0500

    Update ventaTicket.py

--- ventaTicket.py
+++ ventaTicket.py
@@ -8,66 +8,66 @@ app = Flask(__name__)
 
 mysql = MySQL()
 
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Luka12345'
-app.config['MYSQL_DATABASE_DB'] = 'sitio'
+app.config["MYSQL_DATABASE_HOST"] = "localhost"
+app.config["MYSQL_DATABASE_USER"] = "root"
+app.config["MYSQL_DATABASE_PASSWORD"] = "Luka123"
+app.config["MYSQL_DATABASE_DB"] = "sitio"
 mysql.init_app(app)
 
 
-@app.route('/')
+@app.route("/")
 def inicio():
     conexion = mysql.connect()
     cursor = conexion.cursor()
     cursor.execute("SELECT * FROM sitio.eventos")
     eventos = cursor.fetchall()
     conexion.commit()
-    return render_template('index.html', eventos=eventos)
+    return render_template("index.html", eventos=eventos)
 
 
-@app.route('/eventos')
+@app.route("/eventos")
 def admin():
     conexion = mysql.connect()
     cursor = conexion.cursor()
     cursor.execute("SELECT * FROM sitio.eventos")
     eventos = cursor.fetchall()
     conexion.commit()
-    return render_template('eventos.html', eventos=eventos)
+    return render_template("eventos.html", eventos=eventos)
 
 
-@app.route('/guardar', methods=['POST'])
+@app.route("/guardar", methods=["POST"])
 def guardar_eventos():
-    _nombre = request.form['txtNombre']
-    _imagen = request.form['txtImagen']
-    _desc = request.form['txtDesc']
-    _boletos = request.form['txtBoletos']
-    _precio = request.form['txtPre']
+    _nombre = request.form["txtNombre"]
+    _imagen = request.form["txtImagen"]
+    _desc = request.form["txtDesc"]
+    _boletos = request.form["txtBoletos"]
+    _precio = request.form["txtPre"]
 
     sql = f"INSERT INTO `sitio`.`eventos` (`nombre`, `imagen`, `descripcion`, `boletos`,`precio`) VALUES ('{_nombre}', '{_imagen}', '{_desc}', '{_boletos}','{_precio}');"
     conexion = mysql.connect()
     cursor = conexion.cursor()
     cursor.execute(sql)
     conexion.commit()
-    return redirect('/eventos')
+    return redirect("/eventos")
 
 
-@app.route('/ventas')
+@app.route("/ventas")
 def ventas_app():
     conexion = mysql.connect()
     cursor = conexion.cursor()
     cursor.execute("SELECT * FROM sitio.eventos")
     eventos = cursor.fetchall()
     conexion.commit()
-    return render_template('ventas.html', eventos=eventos)
+    return render_template("ventas.html", eventos=eventos)
 
 
-@app.route('/ventas/exito', methods=['POST'])
+@app.route("/ventas/exito", methods=["POST"])
 def ventas_exito():
-    _nombre = request.form['nombre_eventos']
-    _cantidad = request.form['cantidad_boletos']
-    _correo = request.form['correo_']
-    _ciudad = request.form['ciudad_']
-    _codigoPostal = request.form['codigo_postal']
+    _nombre = request.form["nombre_eventos"]
+    _cantidad = request.form["cantidad_boletos"]
+    _correo = request.form["correo_"]
+    _ciudad = request.form["ciudad_"]
+    _codigoPostal = request.form["codigo_postal"]
     codigo = r.randint(0, 2500)
 
     sql = f"INSERT INTO `sitio`.`ventas` (`codigo`, `correo_electronico`, `nombre_evento`, `ciudad`,`codigo_postal`,`precio`) VALUES ('{codigo}', '{_correo}', '{_nombre}', '{_ciudad}','{_codigoPostal}','{0.0}');"
@@ -85,7 +85,11 @@ def ventas_exito():
     cursor.execute("SELECT * FROM sitio.eventos")
     eventos = cursor.fetchall()
 
-    return render_template('ventas.html', eventos=eventos)
+    return render_template("ventas.html", eventos=eventos)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
 if __name__ == '__main__':
